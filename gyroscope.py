1fc802f1165270efa1eaf3b38952445e86ec9359
Bartolomeo <bwilk315@onet.pl>
2022-09-02 18:49:54 +0200

    Initial commit

--- gyroscope.py
+++ gyroscope.py
@@ -1,64 +1,46 @@
 
 from plyer      import gyroscope
-from time       import time
 from math       import degrees
+from sensor     import Sensor
 import logging
 
 
-class Gyroscope:
+class Gyroscope(Sensor):
+    """
+        Management class for gyroscope sensor.
+    """
+    NO_GYRO_VALUE = (.0, .0, .0)
+
     def __init__(self, **kwargs) -> None:
-        """
-            Initializes a 'Gyroscope' class instance.
-            @kwarg show_log:    If enabled, helpful log messages will be displayed in standard output.
-        """
-        self.show_logs  = kwargs.get('show_logs', False)
-        self.__lrt      = time()
-        # Disable logging if needed.
-        if not self.show_logs:
-            logging.disable()
-        # Try to turn gyroscope on.
+        super().__init__(**kwargs)
+
+    def on_disable(self) -> None:
+        gyroscope.disable()
+
+    def on_enable(self) -> None:
         try:
             gyroscope.enable()
         except Exception as e:
             logging.error('Failed to enable the gyroscope on device: %s', e)
-            self.available = False
             return False
         else:
             logging.info('Enabled gyroscope on device.')
-            self.available = True
             return True
-        
-    def __exit__(self, exc_type, exc_value, exc_traceback) -> None:
-        gyroscope.disable()
 
-    def rate(self, deg=False) -> tuple:
+    def rate(self, deg: bool = False) -> tuple:
         """
             Returns gyroscope rotation rate in radians per second [rad/s], if gyroscope
             is unavailable returns empty tuple.
             @param deg: Should method convert returning value to degrees per second [deg/s]? 
         """
-        x, y, z = None, None, None
-        if self.available:
+        try:
             x, y, z = gyroscope.rotation
             if deg:
                 x = degrees(x)
                 y = degrees(y)
                 z = degrees(z)
-        return (x, y, z)
-
-    def change(self, deg=False) -> tuple:
-        """
-            Computes a change of rotation in radians/degrees using delta time based on time
-            interval counted from last call of method to the current.
-            @param deg: Should the return value be in degrees [deg]?
-        """
-        r = self.rate()
-        dt = time() - self.__lrt
-        x, y, z = r[0] * dt, r[1] * dt, r[2] * dt
-        if deg:
-            x = degrees(x)
-            y = degrees(y)
-            z = degrees(z)
-        self.__lrt = time()
-        return (x, y, z)
+            return (x, y, z)
+        except Exception as e:
+            logging.error('Error occured when tried to read gyroscope sensor data: %s', e)
+            return self.NO_GYRO_VALUE
 
